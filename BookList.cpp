bda4cc715c19c54ef2ce2431d7f64ee5234dbc44
Makue672 <2847441167@qq.com>
2025-12-10 17:41:33 +0800

    feat: Update addBook function in BookList.cpp

--- BookList.cpp
+++ BookList.cpp
@@ -2,4 +2,25 @@
 #include "BookList.h"
 #include "globals.h"
 
+BookList::BookList() {
+    head = nullptr;
+    size = 0;
+}
 
+BookList::~BookList() {
+    clear();
+}
+
+void BookList::addBook(const Book& book) {
+    BookNode* newNode = new BookNode(book, nullptr);
+    
+    // 头插法
+    if (head == nullptr) {
+        head = newNode;
+    } else {
+        newNode->next = head;
+        head = newNode;
+    }
+    size++;
+    cout << "Book added successfully!" << endl;
+}
