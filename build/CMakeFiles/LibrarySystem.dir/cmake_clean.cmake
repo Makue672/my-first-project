427f34a3af3f7946ee85287baf6f1a9765fd62c4
Makue672 <2847441167@qq.com>
2025-12-10 18:40:42 +0800

    feat: set up CMake build system

---
+++ build/CMakeFiles/LibrarySystem.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LibrarySystem.dir/src/BookList.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/BookList.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/main.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/main.cpp.obj.d"
+  "CMakeFiles/LibrarySystem.dir/src/menu.cpp.obj"
+  "CMakeFiles/LibrarySystem.dir/src/menu.cpp.obj.d"
+  "LibrarySystem.exe"
+  "LibrarySystem.exe.manifest"
+  "LibrarySystem.pdb"
+  "libLibrarySystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LibrarySystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
